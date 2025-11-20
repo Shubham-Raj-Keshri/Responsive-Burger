9853fcb441fb9fffb588da2b88fc33b60366d3c8
Your Name <your-email@example.com>
2025-11-20 20:23:36 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,100 @@
+const searchForm=document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = ()=>
+{
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+        loginForm.classList.remove('active');
+        navbar.classList.remove('active');
+    
+}
+
+const shoppingCart=document.querySelector('.shopping-cart');
+document.querySelector('#cart-btn').onclick = ()=>
+{
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+
+}
+
+const loginForm=document.querySelector('.login-form');
+document.querySelector('#login-btn').onclick = ()=>
+{
+    loginForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+
+}
+
+
+const navbar=document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = ()=>
+{
+    navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = ()=>
+    {
+        searchForm.classList.remove('active');
+        shoppingCart.classList.remove('active');
+        loginForm.classList.remove('active');
+        navbar.classList.remove('active');
+    }
+    
+
+
+    var swiper = new Swiper(".Product-slider", {
+        loop:true,
+        spaceBetween: 10,
+
+        autoplay:
+        {
+            delay:7500,
+            disableOnInteraction: false,
+        },
+        
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+       
+          },
+          1020: {
+            slidesPerView: 3,
+            
+          },
+        },
+      });
+
+
+      var swiper = new Swiper(".Review-slider", {
+        loop:true,
+        spaceBetween: 10,
+
+        autoplay:
+        {
+            delay:7500,
+            disableOnInteraction: false,
+        },
+        
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+       
+          },
+          1020: {
+            slidesPerView: 3,
+            
+          },
+        },
+      });
